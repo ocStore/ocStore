0edd9939664d251de80d330e63db482a36a211d7
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-11 15:03:21 +0200

    fix php7.0+ and time

--- upload/catalog/controller/event/debug.php
+++ upload/catalog/controller/event/debug.php
@@ -1,21 +1,24 @@
 <?php
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
 class ControllerEventDebug extends Controller {
-	public function before(&$route, &$args) {
-		if ($route == 'common/home') { // add the route you want to test
-			$this->session->data['debug'][$route] = microtime();		
+	public function before(&$route, &$data) {
+		if ($route) {
+			$this->session->data['debug'][$route] = microtime(true);
 		}
 	}
-	
-	public function after($route, &$args, &$output) {
-		if ($route == 'common/home') { // add the route you want to test
+
+	public function after(&$route, &$data, &$output) {
+		if ($route) {
 			if (isset($this->session->data['debug'][$route])) {
-				$log_data = array(
+				$data = array(
 					'route' => $route,
-					'time'  => microtime() - $this->session->data['debug'][$route]
+					'time'  => (round(microtime(true) - $this->session->data['debug'][$route], 3)*1000) . ' mc'
 				);
-				
-				$this->log->write($log_data);
+
+				$this->log->write($data);
 			}
 		}
-	}	
+	}
 }

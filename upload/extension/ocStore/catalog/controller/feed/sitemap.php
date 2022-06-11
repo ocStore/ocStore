aa760c7f8f79e40e4af1461faa8a1b8e71c478a3
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-06-11 18:30:27 +0200

    Add files via upload

--- upload/extension/ocStore/catalog/controller/feed/sitemap.php
+++ upload/extension/ocStore/catalog/controller/feed/sitemap.php
@@ -10,74 +10,243 @@ if (!defined('VERSION')) {
 }
 
 class Sitemap extends \Opencart\System\Engine\Controller {
+	private $setting_default = array(
+		'status' => false,
+		'blog_category_status'   => false,
+		'blog_category_image'    => true,
+		'blog_category_priority' => '0.9',
+		'blog_article_status'    => false,
+		'blog_article_image'     => true,
+		'blog_article_priority'  => '1.0',
+		'category_status'        => true,
+		'category_image'         => true,
+		'category_priority'      => '0.9',
+		'information_status'     => false,
+		'information_image'      => false,
+		'information_priority'   => '0.5',
+		'manufacturer_status'    => false,
+		'manufacturer_image'     => true,
+		'manufacturer_priority'  => '0.7',
+		'product_status'         => true,
+		'product_image'          => true,
+		'product_priority'       => '1.0',
+		'store_id'               => 0,
+		'language'               => 'uk-ua',
+		'language_id'            => 2,
+		'cache_status'           => true,
+		'start'                  => 0,
+		'limit'                  => 10000,
+	);
+	private $setting = array();
+
 	public function index() {
 		if ($this->config->get('feed_sitemap_status')) {
+			$this->setting = $this->setting_default;
+			$setting = $this->config->get('feed_sitemap');
+
+			if ($setting && is_array($setting)) {
+				foreach ($setting as $key => $result) {
+					$this->setting[$key] = $result;
+				}
+			}
+
+			foreach ($this->setting as $key => $result) {
+				if (isset($this->request->get[$key])) {
+					$this->setting[$key] = $this->request->get[$key];
+				}
+			}
+
+			$language_info = $this->db->query("SELECT language_id, code FROM `" . DB_PREFIX . "language` WHERE `language_id` = '" . (int)$this->setting['language_id'] . "'" . (!empty($this->request->get['language']) ? " OR `code` = '" . $this->db->escape($this->setting['language']) . "'" : false))->row;
+
+			if (!empty($language_info['code']) && !empty($language_info['language_id'])) {
+				$this->setting['language'] = $language_info['code'];
+				$this->setting['language_id'] = $language_info['language_id'];
+			} else {
+				$this->setting['language'] = $this->setting_default['language'];
+				$this->setting['language_id'] = $this->setting_default['language_id'];
+			}
+
+			$this->setting['cache_status'] = false;
+
+			if ($this->setting['cache_status']) {
+				$cache_name = 'ocStore.sitemap.' . md5(http_build_query($this->setting));
+				$cache = new \Opencart\System\Library\Cache\File(36000);
+				$output = $cache->get($cache_name);
+
+				if ($output) {
+					$this->response->addHeader('Content-Type: application/xml');
+					$this->response->setOutput($output);
+
+					return '';
+				}
+			}
+
+			$this->load->model('extension/ocStore/feed/sitemap');
+			$this->load->model('tool/image');
+
 			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
 			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
-			$this->load->model('catalog/product');
-			$this->load->model('tool/image');
+			if ($this->setting['blog_category_status'] && is_file(DIR_EXTENSION . 'ocStore/catalog/model/blog/category.php')) {
+				if ($this->setting['blog_category_priority'] > 1) {
+					$this->setting['blog_category_priority'] = 1;
+				}
 
-			$products = $this->model_catalog_product->getProducts();
+				$this->load->model('extension/ocStore/blog/category');
 
-			foreach ($products as $product) {
-				if ($product['image']) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '  <image:image>';
-					$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>';
-					$output .= '  <image:caption>' . $product['name'] . '</image:caption>';
-					$output .= '  <image:title>' . $product['name'] . '</image:title>';
-					$output .= '  </image:image>';
-					$output .= '</url>';
+				$output .= $this->getBlogCategories(0);
+			}
+
+			if ($this->setting['blog_article_status'] && is_file(DIR_EXTENSION . 'ocStore/catalog/model/blog/article.php')) {
+				if ($this->setting['blog_article_priority'] > 1) {
+					$this->setting['blog_article_priority'] = 1;
+				}
+
+				$this->load->model('extension/ocStore/blog/article');
+
+				$articles = $this->model_extension_ocStore_blog_article->getArticles();
+
+				foreach ($articles as $article) {
+					if (!$this->setting['blog_article_image'] || $this->setting['blog_article_image'] && $article['image']) {
+						$output .= '<url>';
+						$output .= '  <loc>' . $this->url->link('extension/ocStor/blog/article', 'article_id=' . $article['article_id'] . '&language=' . $this->setting['language']) . '</loc>';
+						$output .= '  <changefreq>weekly</changefreq>';
+						if (isset($article['date_modified'])) {
+							$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($article['date_modified'])) . '</lastmod>';
+						}
+						$output .= '  <priority>' . $this->setting['blog_article_priority'] . '</priority>';
+
+						if ($this->setting['blog_article_image']) {
+							$output .= '  <image:image>';
+							$output .= '  <image:loc>' . $this->model_tool_image->resize($article['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+							$output .= '  <image:caption>' . $article['name'] . '</image:caption>';
+							$output .= '  <image:title>' . $article['name'] . '</image:title>';
+							$output .= '  </image:image>';
+						}
+
+						$output .= '</url>';
+					}
 				}
 			}
 
-			$this->load->model('catalog/category');
+			if ($this->setting['category_status'] && is_file(DIR_APPLICATION . 'model/catalog/category.php')) {
+				if ($this->setting['category_priority'] > 1) {
+					$this->setting['category_priority'] = 1;
+				}
 
-			$output .= $this->getCategories(0);
+				$this->load->model('catalog/category');
 
-			$this->load->model('catalog/manufacturer');
+				$output .= $this->getCategories(0);
+			}
 
-			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
+			if ($this->setting['information_status'] && is_file(DIR_APPLICATION . 'model/catalog/information.php')) {
+				if ($this->setting['information_priority'] > 1) {
+					$this->setting['information_priority'] = 1;
+				}
 
-			foreach ($manufacturers as $manufacturer) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.7</priority>';
-				$output .= '</url>';
+				$this->load->model('catalog/information');
 
-				$products = $this->model_catalog_product->getProducts(array('filter_manufacturer_id' => $manufacturer['manufacturer_id']));
+				$informations = $this->model_catalog_information->getInformations();
+
+				foreach ($informations as $information) {
+					if (!$this->setting['information_image'] || $this->setting['information_image'] && $information['image']) {
+						$output .= '<url>';
+						$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id'] . '&language=' . $this->setting['language']) . '</loc>';
+						$output .= '  <changefreq>weekly</changefreq>';
+						if (isset($information['date_modified'])) {
+							$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($information['date_modified'])) . '</lastmod>';
+						}
+						$output .= '  <priority>' . $this->setting['information_priority'] . '</priority>';
+
+						if ($this->setting['information_image']) {
+							$output .= '  <image:image>';
+							$output .= '  <image:loc>' . $this->model_tool_image->resize($information['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+							if (!isset($information['name'])) {
+								$information['name'] = $information['title'];
+							}
+							$output .= '  <image:caption>' . $information['name'] . '</image:caption>';
+							$output .= '  <image:title>' . $information['name'] . '</image:title>';
+							$output .= '  </image:image>';
+						}
+
+						$output .= '</url>';
+					}
+				}
+			}
+
+			if ($this->setting['manufacturer_status'] && is_file(DIR_APPLICATION . 'model/catalog/manufacturer.php')) {
+				if ($this->setting['manufacturer_priority'] > 1) {
+					$this->setting['manufacturer_priority'] = 1;
+				}
+				
+				$this->load->model('catalog/manufacturer');
+
+				$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
+
+				foreach ($manufacturers as $manufacturer) {
+					if (!$this->setting['manufacturer_image'] || $this->setting['manufacturer_image'] && $manufacturer['image']) {
+						$output .= '<url>';
+						$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id'] . '&language=' . $this->setting['language']) . '</loc>';
+						$output .= '  <changefreq>weekly</changefreq>';
+						if (isset($manufacturer['date_modified'])) {
+							$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($manufacturer['date_modified'])) . '</lastmod>';
+						}
+						$output .= '  <priority>' . $this->setting['manufacturer_priority'] . '</priority>';
+
+						if ($this->setting['manufacturer_image']) {
+							$output .= '  <image:image>';
+							$output .= '  <image:loc>' . $this->model_tool_image->resize($manufacturer['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+							$output .= '  <image:caption>' . $manufacturer['name'] . '</image:caption>';
+							$output .= '  <image:title>' . $manufacturer['name'] . '</image:title>';
+							$output .= '  </image:image>';
+						}
+
+						$output .= '</url>';
+					}
+				}
+			}
+
+			if ($this->setting['product_status'] && is_file(DIR_APPLICATION . 'model/catalog/product.php')) {
+				if ($this->setting['product_priority'] > 1) {
+					$this->setting['product_priority'] = 1;
+				}
+
+				$this->load->model('catalog/product');
+
+				$products = $this->model_catalog_product->getProducts();
 
 				foreach ($products as $product) {
-					$output .= '<url>';
-					$output .= '  <loc>' . $this->url->link('product/product', 'manufacturer_id=' . $manufacturer['manufacturer_id'] . '&product_id=' . $product['product_id']) . '</loc>';
-					$output .= '  <changefreq>weekly</changefreq>';
-					$output .= '  <priority>1.0</priority>';
-					$output .= '</url>';
+					if (!$this->setting['product_image'] || $this->setting['product_image'] && $product['image']) {
+						$output .= '<url>';
+						$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id'] . '&language=' . $this->setting['language']) . '</loc>';
+						$output .= '  <changefreq>weekly</changefreq>';
+						if (isset($product['date_modified'])) {
+							$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($product['date_modified'])) . '</lastmod>';
+						}
+						$output .= '  <priority>' . $this->setting['product_priority'] . '</priority>';
+
+						if ($this->setting['product_image']) {
+							$output .= '  <image:image>';
+							$output .= '  <image:loc>' . $this->model_tool_image->resize($product['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+							$output .= '  <image:caption>' . $product['name'] . '</image:caption>';
+							$output .= '  <image:title>' . $product['name'] . '</image:title>';
+							$output .= '  </image:image>';
+						}
+
+						$output .= '</url>';
+					}
 				}
-			}
-
-			$this->load->model('catalog/information');
-
-			$informations = $this->model_catalog_information->getInformations();
-
-			foreach ($informations as $information) {
-				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>';
-				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>0.5</priority>';
-				$output .= '</url>';
 			}
 
 			$output .= '</urlset>';
 
-			$this->response->addHeader('Content-Type: application/xml');
-			$this->response->setOutput($output);
+			if ($output) {
+				if ($this->setting['cache_status']) {
+					$cache->set($cache_name, $output);
+				}
+				$this->response->addHeader('Content-Type: application/xml');
+				$this->response->setOutput($output);
+			}
 		}
 	}
 
@@ -93,19 +262,23 @@ class Sitemap extends \Opencart\System\Engine\Controller {
 				$new_path = $current_path . '_' . $result['category_id'];
 			}
 
-			$output .= '<url>';
-			$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $new_path) . '</loc>';
-			$output .= '  <changefreq>weekly</changefreq>';
-			$output .= '  <priority>0.7</priority>';
-			$output .= '</url>';
-
-			$products = $this->model_catalog_product->getProducts(array('filter_category_id' => $result['category_id']));
-
-			foreach ($products as $product) {
+			if (!$this->setting['category_image'] || $this->setting['category_image'] && $result['image']) {
 				$output .= '<url>';
-				$output .= '  <loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id']) . '</loc>';
+				$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $new_path . '&language=' . $this->setting['language']) . '</loc>';
 				$output .= '  <changefreq>weekly</changefreq>';
-				$output .= '  <priority>1.0</priority>';
+				if (isset($result['date_modified'])) {
+					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($result['date_modified'])) . '</lastmod>';
+				}
+				$output .= '  <priority>' . $this->setting['category_priority'] . '</priority>';
+
+				if ($this->setting['category_image']) {
+					$output .= '  <image:image>';
+					$output .= '  <image:loc>' . $this->model_tool_image->resize($result['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+					$output .= '  <image:caption>' . $result['name'] . '</image:caption>';
+					$output .= '  <image:title>' . $result['name'] . '</image:title>';
+					$output .= '  </image:image>';
+				}
+
 				$output .= '</url>';
 			}
 
@@ -114,47 +287,11 @@ class Sitemap extends \Opencart\System\Engine\Controller {
 
 		return $output;
 	}
-	
-	public function index22() {
-		if ($this->config->get('blog_sitemap_status')) {
-			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
-			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
-			$this->load->model('blog/article');
-			$this->load->model('tool/image');
-
-			$articles = $this->model_blog_article->getArticles();
-
-			foreach ($articles as $article) {
-				if ($article['image']) {
-					$output .= '<url>';
-					$output .= '<loc>' . $this->url->link('blog/article', 'article_id=' . $article['article_id']) . '</loc>';
-					$output .= '<changefreq>weekly</changefreq>';
-					$output .= '<priority>1.0</priority>';
-					$output .= '<image:image>';
-					$output .= '<image:loc>' . $this->model_tool_image->resize($article['image'], $this->config->get($this->config->get('config_theme') . '_image_popup_width'), $this->config->get($this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>';
-					$output .= '<image:caption>' . $article['name'] . '</image:caption>';
-					$output .= '<image:title>' . $article['name'] . '</image:title>';
-					$output .= '</image:image>';
-					$output .= '</url>';
-				}
-			}
-
-			$this->load->model('blog/category');
-
-			$output .= $this->getCategories(0);
-
-			$output .= '</urlset>';
-
-			$this->response->addHeader('Content-Type: application/xml');
-			$this->response->setOutput($output);
-		}
-	}
-
-	protected function getCategories22($parent_id, $current_path = '') {
+	protected function getBlogCategories($parent_id, $current_path = '') {
 		$output = '';
 
-		$results = $this->model_blog_category->getCategories($parent_id);
+		$results = $this->model_extension_ocStore_blog_category->getCategories($parent_id);
 
 		foreach ($results as $result) {
 			if (!$current_path) {
@@ -163,11 +300,25 @@ class Sitemap extends \Opencart\System\Engine\Controller {
 				$new_path = $current_path . '_' . $result['blog_category_id'];
 			}
 
-			$output .= '<url>';
-			$output .= '<loc>' . $this->url->link('blog/category', 'blog_category_id=' . $new_path) . '</loc>';
-			$output .= '<changefreq>weekly</changefreq>';
-			$output .= '<priority>0.7</priority>';
-			$output .= '</url>';
+			if (!$this->setting['blog_category_image'] || $this->setting['blog_category_image'] && $result['image']) {
+				$output .= '<url>';
+				$output .= '  <loc>' . $this->url->link('extension/ocStor/blog/category', 'blog_category_id=' . $new_path . '&language=' . $this->setting['language']) . '</loc>';
+				$output .= '  <changefreq>weekly</changefreq>';
+				if (isset($result['date_modified'])) {
+					$output .= '  <lastmod>' . date('Y-m-d\TH:i:sP', strtotime($result['date_modified'])) . '</lastmod>';
+				}
+				$output .= '  <priority>' . $this->setting['blog_category_priority'] . '</priority>';
+
+				if ($this->setting['blog_category_image']) {
+					$output .= '  <image:image>';
+					$output .= '  <image:loc>' . $this->model_tool_image->resize($result['image'], $this->config->get('config_image_popup_width'), $this->config->get('config_image_popup_height')) . '</image:loc>';
+					$output .= '  <image:caption>' . $result['name'] . '</image:caption>';
+					$output .= '  <image:title>' . $result['name'] . '</image:title>';
+					$output .= '  </image:image>';
+				}
+
+				$output .= '</url>';
+			}
 
 			$output .= $this->getCategories($result['blog_category_id'], $new_path);
 		}
--- upload/extension/ocStore/catalog/model/feed/sitemap.php
+++ upload/extension/ocStore/catalog/model/feed/sitemap.php
@@ -10,53 +10,14 @@ if (!defined('VERSION')) {
 }
 
 class Sitemap extends \Opencart\System\Engine\Model {
-	public function getCategory() {
-		$query = $this->db->query("SELECT cd.name, c.category_id, c.parent_id FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' AND c.sort_order <> '-1'");
-
-		return $query->rows;
+	public function getLanguage(array $data = []): array {
+		return $this->db->query("SELECT language_id, code FROM `" . DB_PREFIX . "language` WHERE `language_id` = '" . (int)$data['language_id'] . "'" . (!empty($data['language']) ? " OR `code` = '" . $this->db->escape($data['language']) . "'" : false))->row;
 	}
 
-	public function getProduct($allowed_categories, $allowed_manufacturers, $out_of_stock_id, $vendor_required = true, $bus_image = true, $bus_image_quantity = '10', $bus_main_category = true, $bus_quantity_status = false) {
-		$sql = "SELECT p2c.category_id, p.product_id, p.model, p.sku, p.upc, p.ean, p.jan, p.isbn, p.mpn, p.location, p.quantity, p.stock_status_id, p.image, p.tax_class_id, pd.name, pd.description, pd.meta_title, pd.meta_description, pd.meta_keyword, pd.meta_h1,";
-
-		if ((int)$bus_image_quantity > '1') {
-			$sql .= " (SELECT SUBSTRING_INDEX(GROUP_CONCAT(pi.image ORDER BY pi.image SEPARATOR ','), ',', " . ((int)$bus_image_quantity <= "10" ? (int)$bus_image_quantity - 1 : "9") . ") FROM " . DB_PREFIX . "product_image pi WHERE pi.product_id = p2c.product_id) AS images,";
-		}
-
-		$sql .= " m.name AS manufacturer, p.price AS price, ps.price AS special FROM " . DB_PREFIX . "product_to_category AS p2c JOIN " . DB_PREFIX . "product AS p ON (p.product_id = p2c.product_id) " . ($vendor_required ? '' : 'LEFT ') . "JOIN " . DB_PREFIX . "manufacturer AS m ON (m.manufacturer_id = p.manufacturer_id) LEFT JOIN " . DB_PREFIX . "product_description AS pd ON (pd.product_id = p2c.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store AS p2s ON (p2s.product_id = p2c.product_id) LEFT JOIN " . DB_PREFIX . "product_special AS ps ON (ps.product_id = p2c.product_id) AND ps.customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND ps.date_start < NOW() AND (ps.date_end = '0000-00-00' OR ps.date_end > NOW())";
-
-		if (!empty($allowed_categories)) {
-			$sql .= " WHERE p2c.category_id IN (" . $this->db->escape($allowed_categories) . ")";
-		} else {
-			$sql .= " WHERE p2c.category_id";
-		}
-
-		if (!empty($bus_main_category)) {
-			$sql .= " AND p2c.main_category = '1'";
-		}
-
-		if (!empty($allowed_manufacturers)) {
-			$sql .= " AND m.manufacturer_id IN (" . $this->db->escape($allowed_manufacturers) . ")";
-		}
-
-		$sql .= " AND p.status = '1'";
-
-		if (!empty($bus_image)) {
-			$sql .= " AND p.image != ''";
-		}
-
-		$sql .= " AND p.date_available <= NOW() AND (p.quantity " . (!empty($bus_quantity_status) ? '>=' : '>') . " '0' OR p.stock_status_id != '" . (int)$out_of_stock_id . "') AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "'";
-
-		$sql .= " GROUP BY p2c.product_id";
-
-		$query = $this->db->query($sql);
-
-		return $query->rows;
-	}
-
-	public function getProductAttributes($product_id) {
-		$query = $this->db->query("SELECT ad.name, pa.text FROM " . DB_PREFIX . "product_attribute pa LEFT JOIN " . DB_PREFIX . "attribute a ON (pa.attribute_id = a.attribute_id) LEFT JOIN " . DB_PREFIX . "attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE pa.product_id = '" . (int)$product_id . "' AND ad.language_id = '" . (int)$this->config->get('config_language_id') . "' AND pa.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY a.sort_order, ad.name");
-
-		return $query->rows;
-	}
+	public function getBlogCategories(array $data = []): array {}
+	public function getBlogArticles(array $data = []): array {}
+	public function getCategories(array $data = []): array {}
+	public function getInformations(array $data = []): array {}
+	public function getManufacturers(array $data = []): array {}
+	public function getProducts(array $data = []): array {}
 }
